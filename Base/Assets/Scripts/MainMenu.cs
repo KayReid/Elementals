20377d1e825a265979b513e972d7b27af5fec641
Binh Vo <binhvo07@gmail.com>
2018-02-23 08:51:44 +0100

    Added main menu. Menu can now load new game, continue with the selected scene

---
+++ Base/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    
+    public string restartLevel;
+    public string continueLevel;
+
+    public string[] levelNames;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NewGame() {
+        SceneManager.LoadScene(restartLevel);    
+    }
+    public void ContinueGame() {
+        SceneManager.LoadScene(continueLevel);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+
+}
