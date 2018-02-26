5ea2a89f958d8675c910e2b16283a1084afa8164
celia96 <celiachoy96@gmail.com>
2018-02-27 00:32:02 +0100

    Snowball script

--- Base/Assets/Scripts/SnowBall.cs
+++ Base/Assets/Scripts/SnowBall.cs
@@ -23,24 +23,27 @@ public class SnowBall : MonoBehaviour {
 
 	void Update ()
 	{
-		if (check.IsTouchingLayers (shieldLayers)) {
-			print ("touch");
-			direction.x *= 1;
+		
+		if (check != null) {
+			if (check.IsTouchingLayers (shieldLayers)) {
+				direction.x = 1;
+				print ("touch");
+				Destroy (gameObject);
+			}
+			transform.position += new Vector3 (direction.x, 0, 0) * speed * Time.deltaTime;
+		}
+	}
+		
+
+	void OnCollisionStay2D(Collision2D col)
+	{
+		if (col.collider.CompareTag("Player"))
+		{
+			Player player = col.transform.root.GetComponentInChildren<Player>();
+			player.Die();
 			Destroy (gameObject);
 		}
-		transform.position += new Vector3 (direction.x, 0, 0) * speed * Time.deltaTime;
 	}
-
-
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.CompareTag ("Player")) { // This checks if we hit an asteroid. The asteroid needs the "Asteroid" tag for this to work!!
-			Player player = other.GetComponent<Player> (); // Grab the asteroid script from the hit GameObject
-			player.Die (); // notify the asteroid it got hit
-			Destroy (gameObject); // Destory this projectile
-		}
-	}
-
 
 	/// <summary>
 	/// Destroys the projectile after seconds. This is a coroutine that needs be started using StartCoroutine().
