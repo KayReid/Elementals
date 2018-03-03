4a285a2018ee5987260058c77c6c22a00c1975bc
celia96 <celiachoy96@gmail.com>
2018-03-03 16:44:33 +0100

    Cleaned Up folders And added a story

---
+++ Base/Assets/Scripts/StoryTelling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryTelling : MonoBehaviour {
+
+	[TextArea (4, 30)]
+	public string[] page;
+	int PageNumber = -1;
+	Text view;
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		view = GetComponent<Text> ();
+		Invoke ("PageTurn", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			print ("key pressed");
+			Invoke ("PageTurn", 0.2f);
+		}
+
+	}
+
+	void PageTurn() {
+		PageNumber++;
+		if (PageNumber < page.Length) {
+			view.text = page [PageNumber];
+		
+		} else {
+			SceneManager.LoadScene(levelToLoad);
+		}
+	
+	}
+}
+
